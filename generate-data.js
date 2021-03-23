bf9abf8c9ca8cf1690dc0099abcfd68379d813ef
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-03-24 00:12:31 +0700

    07 - random products data

--- generate-data.js
+++ generate-data.js
@@ -24,15 +24,43 @@ const randomCategoryList = (n) => {
   return categoryList;
 };
 
+const randomProductList = (categoryList, numberOfProducts) => {
+  if (numberOfProducts <= 0) return [];
+
+  const productList = [];
+
+  // random data
+  for (const category of categoryList) {
+    Array.from(new Array(numberOfProducts)).forEach(() => {
+      const product = {
+        categoryId: category.id,
+        id: faker.random.uuid(),
+        name: faker.commerce.productName(),
+        color: faker.commerce.color(),
+        price: faker.commerce.price(),
+        description: faker.commerce.productDescription(),
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        thumbnailUrl: faker.image.imageUrl(400, 400),
+      };
+
+      productList.push(product);
+    });
+  }
+
+  return productList;
+};
+
 // IFFE
 (() => {
   // random data
   const categoryList = randomCategoryList(4);
+  const productList = randomProductList(categoryList, 5);
 
   // prepare db object
   const db = {
     categories: categoryList,
-    products: [],
+    products: productList,
     profile: {
       name: "Po",
     },
