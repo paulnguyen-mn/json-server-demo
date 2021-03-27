e6a2484a4b0ce5483dbce554d3d1df8f6aa98c4b
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-03-27 17:26:49 +0700

    09 - Auto add createdAt field
    - Config prettier

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
 
 // Add custom routes before JSON Server router
-server.get("/echo", (req, res) => {
+server.get('/echo', (req, res) => {
   res.jsonp(req.query);
 });
 
@@ -15,8 +15,10 @@ server.get("/echo", (req, res) => {
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     req.body.createdAt = Date.now();
+    req.body.updatedAt = Date.now();
+  } else if (req.method === 'PATCH') {
     req.body.updatedAt = Date.now();
   }
 
@@ -25,7 +27,7 @@ server.use((req, res, next) => {
 });
 
 // Use default router
-server.use("/api", router);
+server.use('/api', router);
 server.listen(3000, () => {
-  console.log("JSON Server is running");
+  console.log('JSON Server is running');
 });
