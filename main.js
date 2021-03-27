5fc37909760f92f80e75935046537e8492fa55de
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-03-27 18:30:34 +0700

    13 - Update PORT using env var

--- main.js
+++ main.js
@@ -55,6 +55,9 @@ router.render = (req, res) => {
 
 // Use default router
 server.use('/api', router);
-server.listen(3000, () => {
+
+// Start server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log('JSON Server is running');
 });
