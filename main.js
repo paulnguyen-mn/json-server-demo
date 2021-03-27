5d293d9572a7b0f8307d0a8309a45415a1be5589
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-03-27 18:01:05 +0700

    10 - Code custom output

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const jsonServer = require('json-server');
+const queryString = require('query-string');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
@@ -25,6 +26,32 @@ server.use((req, res, next) => {
   // Continue to JSON Server router
   next();
 });
+
+// Custom output for LIST with pagination
+router.render = (req, res) => {
+  // Check GET with pagination
+  // If yes, custom output
+  const headers = res.getHeaders();
+
+  const totalCountHeader = headers['x-total-count'];
+  if (req.method === 'GET' && totalCountHeader) {
+    const queryParams = queryString.parse(req._parsedUrl.query);
+
+    const result = {
+      data: res.locals.data,
+      pagination: {
+        _page: Number.parseInt(queryParams._page) || 1,
+        _limit: Number.parseInt(queryParams._limit) || 10,
+        _totalRows: Number.parseInt(totalCountHeader),
+      },
+    };
+
+    return res.jsonp(result);
+  }
+
+  // Otherwise, keep default behavior
+  res.jsonp(res.locals.data);
+};
 
 // Use default router
 server.use('/api', router);
